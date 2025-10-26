3b4ba016bf83c432bdd42a1a2f86fb6a120e8a58
MAMIDI RAVI PRAVEEN <147956894+RAVI-PRAVEEN-1@users.noreply.github.com>
2025-10-26 23:12:45 +0530

    Create multiplication.py

---
+++ py_programs/multiplication.py
@@ -0,0 +1,11 @@
+# Program to multiply two numbers
+
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiply the numbers
+product = num1 * num2
+
+# Display the result
+print("The product of", num1, "and", num2, "is:", product)
